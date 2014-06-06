78aa1af4b90968cc0df81b57377937fb7e4370ef
Lars Bergstrom <lars@lars.com>
2014-06-06 10:42:45 -0500

    Servo version of Rust travis config

---
+++ travis.osx.before_script.sh
@@ -0,0 +1,1 @@
+./configure --disable-docs --disable-optimize-tests --llvm-root=`pwd`/clang+llvm-3.4.1-x86_64-apple-darwin10.9 --enable-fast-make --enable-clang
---
+++ travis.osx.install.deps.sh
@@ -0,0 +1,4 @@
+brew update
+brew install gcc
+wget http://llvm.org/releases/3.4.1/clang+llvm-3.4.1-x86_64-apple-darwin10.9.tar.xz
+tar zxvf clang+llvm-3.4.1-x86_64-apple-darwin10.9.tar.xz
